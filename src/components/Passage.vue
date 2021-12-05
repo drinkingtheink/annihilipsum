8bcbf8190558ab956f30015d3fefa4b7d1acd06d
drinkingtheink <jason.m.harrison@gmail.com>
2021-12-05 00:03:05 -0600

    progress on passage reveal

--- src/components/Passage.vue
+++ src/components/Passage.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="the-passage">
         <p>
-            <span v-for="word in passageInWords" :key="word">
+            <span 
+                v-for="word, index in passageInWords" 
+                :key="word" 
+                class="word"
+                v-bind:style="{ animationDelay: index + '00ms' }"
+            >
                 {{ word }}
             </span>
         </p>
@@ -14,6 +19,11 @@ export default {
     props: {
         passage: String,
         passageInWords: Array
+    },
+    methods: {
+        genAniDelay() {
+            return `0.4s`
+        }
     }
 }
 </script>
@@ -22,5 +32,20 @@ export default {
 .the-passage p {
   font-family: 'Smooch', cursive;
   font-size: 2.25rem;
+}
+
+.word {
+    animation: appear;
+    animation-duration: 2s;
+    animation-fill-mode: initial;
+}
+
+@keyframes appear {
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
 }
 </style>
