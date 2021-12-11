f30a8348396539a1021031f9ea4cffc727fdd4fd
drinkingtheink <jason.m.harrison@gmail.com>
2021-12-10 21:08:42 -0600

    error screen flicker

--- src/components/AppStage.vue
+++ src/components/AppStage.vue
@@ -32,7 +32,7 @@
           </div>
         </section>
 
-        <section class="generated">
+        <section class="generated" :class="{ 'no-content': !this.formattedFinalText}">
             <div v-if="!formattedFinalText" class="toolbox-sr-logo">
               <SRLogo class="toolbox-sr" />
               <p>Something went wrong, try a different request.<br/> - The Southern Reach</p>
@@ -294,7 +294,7 @@ label {
 }
 
 .generated {
-  background-color: #e0e0e0;
+  background-color: #e8e8e8;
   margin: 1rem 0 0.5rem 0;
   padding: 0.5rem 1.5rem;
   border-radius: 10px;
@@ -311,6 +311,105 @@ label {
   margin: 0;
   padding: 0;
   font-family: 'Roboto Mono', monospace;
+}
+
+.generated.no-content::before {
+  content: " ";
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: linear-gradient(rgba(18, 16, 16, 0) 50%, rgba(0, 0, 0, 0.25) 50%), linear-gradient(90deg, rgba(255, 0, 0, 0.06), rgba(0, 255, 0, 0.02), rgba(0, 0, 255, 0.06));
+  z-index: 2;
+  background-size: 100% 2px, 3px 100%;
+  pointer-events: none;
+}
+
+@keyframes flicker {
+  0% {
+  opacity: 0.27861;
+  }
+  5% {
+  opacity: 0.34769;
+  }
+  10% {
+  opacity: 0.23604;
+  }
+  15% {
+  opacity: 0.90626;
+  }
+  20% {
+  opacity: 0.18128;
+  }
+  25% {
+  opacity: 0.83891;
+  }
+  30% {
+  opacity: 0.65583;
+  }
+  35% {
+  opacity: 0.67807;
+  }
+  40% {
+  opacity: 0.26559;
+  }
+  45% {
+  opacity: 0.84693;
+  }
+  50% {
+  opacity: 0.96019;
+  }
+  55% {
+  opacity: 0.08594;
+  }
+  60% {
+  opacity: 0.20313;
+  }
+  65% {
+  opacity: 0.71988;
+  }
+  70% {
+  opacity: 0.53455;
+  }
+  75% {
+  opacity: 0.37288;
+  }
+  80% {
+  opacity: 0.71428;
+  }
+  85% {
+  opacity: 0.70419;
+  }
+  90% {
+  opacity: 0.7003;
+  }
+  95% {
+  opacity: 0.36108;
+  }
+  100% {
+  opacity: 0.24387;
+  }
+}
+
+.generated.no-content {
+  background-color: white;
+}
+
+.generated.no-content::after {
+  content: " ";
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(18, 16, 16, 0.1);
+  opacity: 0;
+  z-index: 2;
+  pointer-events: none;
+  animation: flicker 0.15s infinite;
 }
 
 .generated .text-actions {
